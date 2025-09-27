1f1ee32a21ecb855b2bd619c25ffbb1d01dcf8e1
Serhii <smyshynskyi@gmail.com>
2025-09-27 23:43:23 +0300

    add 2 exercise in homework4

--- Les4/HomeWork4.py
+++ Les4/HomeWork4.py
@@ -10,3 +10,16 @@ print(numbers)
 # Способ 2
 numbers = [num for num in range(100) if num % 2 == 0 and num % 3 == 0]
 print(numbers)
+
+# Задание 2. Имеется список items = [1.2, 3, None, 100, {'info': 'bla-bla'}, 44, 'Hi!', 99,
+# 44.32, None]. Составьте новый список numbers, который содержит только целые числа (int) и
+# вещественные числа (float) из списка items. Выведите на экран сумму чисел в numbers.
+items = [1.2, 3, None, 100, {'info': 'bla-bla'}, 44, 'Hi!', 99, 44.32, None]
+numbers = []
+sum = 0
+for item in items:
+    if type(item) == int or type(item) == float:
+        numbers.append(item)
+        sum += item
+print(numbers)
+print(f"Сумма всех чисел из списка = {sum}")
