e6521dde678d3f82ddccdd0a962ccf23318d3ac6
Serhii <smyshynskyi@gmail.com>
2025-10-03 12:30:13 +0300

    add 2 exercise

--- Les4/AdditionalHW.py
+++ Les4/AdditionalHW.py
@@ -11,3 +11,19 @@ print(unilist)
 # Задание 2. Создайте переменную text, значение которой определяется с помощью ввода данных с
 # клавиатуры. Выведите список с длинами каждого слова в строке text. Слова в тексте разделяются
 # с помощью пробелов и знаков препинания.
+text = input("Enter any text:")
+signs = [' ', '-', ':', ',', '.', ';', '?', '!', '_', '-', "'", '"', '/', '&', '$', '@', '+']
+count_letters = []
+count = 0
+for c in text:
+    if c not in signs:
+        count += 1
+    elif c in signs and count !=0:
+        count_letters.append(count)
+        count = 0
+    else:
+        print("В тексте непредвиденная ошибка")
+        break
+if count != 0:
+    count_letters.append(count)
+print(count_letters)
